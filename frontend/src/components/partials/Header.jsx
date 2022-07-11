838bfd6d3b7abe433377dcf3dafaa6d68a908449
KevinHe23 <kevinherrera2324@gmail.com>
2022-07-10 21:19:30 -0600

    Actualizaciones styles header y footer

--- frontend/src/components/partials/Header.jsx
+++ frontend/src/components/partials/Header.jsx
@@ -3,22 +3,29 @@ import {Link} from "react-router-dom";
 function Header(){
     return(
         <header>
+            <div className="container">
+    <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
         
-        <div className="container">
-        <header className="d-flex justify-content-center py-3">
-        <ul class="nav nav-pills">
-        <li class="nav-item">
+      </a>
+
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+      <li class="nav-item">
             <Link to = "/" class="nav-link active"  aria-current="page">Home</Link>
         </li>
         <li className="nav-item">
             <Link to ="/registro" class="nav-link">Registro</Link>
         </li>
-       
       </ul>
 
-      
+      <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-primary me-2">Login</button>
+        <button type="button" class="btn btn-primary">Sign-up</button>
+      </div>
     </header>
-    </div>
+  </div>
+        
+       
 
 
 
