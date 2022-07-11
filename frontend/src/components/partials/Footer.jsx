36c0674b1fcbcb9b817af35b882085daee5b9bee
KevinHe23 <kevinherrera2324@gmail.com>
2022-07-10 19:16:28 -0600

    actualizaciones en header y footer

--- frontend/src/components/partials/Footer.jsx
+++ frontend/src/components/partials/Footer.jsx
@@ -1,23 +1,24 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function Footer(){
-    return(
-        <footer>
-            <div className="container">
-                <footer className="py-3 my-4">
+function Footer() {
+    return (
+        <footer >
+
+            <div class="container">
+                <footer class="py-3 my-4">
                     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                        <li className="nav-item">
-                            <Link to="/" class="nav-link px-2 text-muted">Home</Link>
+                        <li class="nav-item">
+                            <Link to="#" class="nav-link px-2 text-muted">Home</Link>
                         </li>
-                        <li className="nav-item">
+                        <li class="nav-item">
                             <Link to="/registro" class="nav-link px-2 text-muted">Registro</Link>
                         </li>
-                       
                     </ul>
-                     <p className="text-center text-muted">&copy; 2022 Company, Inc</p>
+                    <p class="text-center text-muted">&copy; 2021 Company, Inc</p>
                 </footer>
             </div>
+
         </footer>
     );
 }
