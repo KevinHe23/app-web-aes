ec3c0e735e4e49fa6537bb652649b0524105104b
KevinHe23 <kevinherrera2324@gmail.com>
2022-07-15 22:24:04 -0600

    pagina registro terminada, listo para servidor

--- frontend/src/components/Paginas/Registro.jsx
+++ frontend/src/components/Paginas/Registro.jsx
@@ -1,30 +1,87 @@
+import { useState } from "react";
 import React from "react";
+import axios from "axios"
 
 function Registro() {
+    //declaracion objeto inicial
+    const [input, setInput] = useState({
+        nombres: "",
+        email: "",
+        contraseña: ""
+    });
+    //Cambia el valor por el que escribe el usuario
+    function handleChange(event) {
+        console.log(input);
+        const { name, value } = event.target;
+        //Guarda el valor previo
+        setInput(prevInput => {
+            return {
+                ...prevInput,
+                [name]: value
+            }
+        });
+    }
+    //se activa cuando se oprime el boton
+    function handleClick(event) {
+        // evita el parpadeo predeterminado
+        event.preventDefault();
+
+        //crea objeto para pasar al servidor
+        const nUsuario = {
+            nombres: input.nombres,
+            email: input.email,
+            contraseña: input.contraseña
+        }
+
+        //pasar datos a servidor o bd
+        axios.post("/registrar", nUsuario)
+    }
+
+
     return (
 
         <div className="container micontenedor">
             <main class="form-signin w-100 m-auto">
                 <form>
-                    <img classNamme="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
-                        <h1 className="h3 mb-3 fw-normal" >Registrese</h1>
 
-                        <div className="form-floating">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
-                                <label for="floatingInput">correo electrónico</label>
-                        </div>
-                        <div className="form-floating">
-                            <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-                                <label for="floatingPassword">contraseña</label>
-                        </div>
+                    <h1 className="h3 mb-3 fw-normal" >Por favor, registrese</h1>
+                    <div className="form-floating">
+                        <input
+                            onChange={handleChange}
+                            name="nombres"
+                            value={input.name}
+                            type="text"
+                            class="form-control"
+                            id="floatingName"
+                            placeholder="Nombre completo" />
+                        <label for="floatingInput">Nombre completo</label>
+                    </div>
 
-                        <div className="checkbox mb-3">
-                            <label>
-                                <input type="checkbox" value="remember-me"/> Recuerdame
-                            </label>
-                        </div>
-                        <button className="w-100 btn btn-lg btn-primary" type="submit">Registrarse</button>
-                        <p className="mt-5 mb-3 text-muted">&copy; 2022</p>
+                    <div className="form-floating">
+                        <input
+                            onChange={handleChange}
+                            name="email"
+                            value={input.email}
+                            type="email"
+                            class="form-control"
+                            id="floatingInput"
+                            placeholder="name@example.com" />
+                        <label for="floatingInput">Correo electrónico</label>
+                    </div>
+                    <div className="form-floating">
+                        <input
+                            onChange={handleChange}
+                            name="contraseña"
+                            value={input.contraseña}
+                            type="password"
+                            className="form-control"
+                            id="floatingPassword"
+                            placeholder="Password" />
+                        <label for="floatingPassword">Contraseña</label>
+                    </div>
+
+                    <button onClick={handleClick} className="w-100 btn btn-lg btn-primary" type="submit">Registrarse</button>
+
                 </form>
             </main>
 
